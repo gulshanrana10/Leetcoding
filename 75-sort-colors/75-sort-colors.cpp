3cd08dc6fd5c3f4fed51b129dfdebb62ea1db6cc
Gulshan Rana <39074166+gulshanrana10@users.noreply.github.com>
2022-01-08 15:37:26 +0530

    Time: 5 ms (8.54%), Space: 8.3 MB (68.33%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        /*
         for(int i=0;i<nums.size()-1;i++){
             for(int j=1+i;j<nums.size();j++){
                 if(nums[i]>nums[j]){
@@ -10,6 +11,29 @@ public:
                 }
             }
         }
+        */
+        int z=0,o=0,t=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0) z++;
+           else if(nums[i]==1) o++;
+           else if(nums[i]==2) t++;
+        }
+       int a=nums.size();
+         for(int i=0;i<a;i++){
+             nums.pop_back();
+         }
+
+        for(int i=0;i<z;i++){
+             nums.push_back(0);
+         }
+        for(int i=0;i<o;i++){
+             nums.push_back(1);
+         }
+        for(int i=0;i<t;i++){
+             nums.push_back(2);
+         }
+        
+        
         
     }
 };
