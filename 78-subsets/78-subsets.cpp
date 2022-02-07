998631beb81a2dca316bf449fbb656b15fb34b36
Gulshan Rana <39074166+gulshanrana10@users.noreply.github.com>
2022-02-07 19:50:26 +0530

    Time: 26 ms (5.28%), Space: 16.8 MB (7.84%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void process(vector<int> temp1,vector<int> temp,int i){
-        if(i==temp1.size()){
+    void process(vector<int> nums,vector<int> temp,int i){
+        if(i==nums.size()){
             ans.push_back(temp);
             return;
         }
         
-        temp.push_back(temp1[i]);
-        process(temp1,temp,i+1);
+        temp.push_back(nums[i]);
+        process(nums,temp,i+1);
         temp.pop_back();
-        process(temp1,temp,i+1);
+        process(nums,temp,i+1);
         
     }
+
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int> temp;
-        vector<int> temp1;
-        set<int> nums1;
-    for(int i=0;i<nums.size();i++)nums1.insert(nums[i]);
-        for(auto x:nums1)temp1.push_back(x);
-        process(temp1,temp,0);
+    
+        process(nums,temp,0);
         return ans;
     }
 };
