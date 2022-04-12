6079dc5fa26b6bf002e6f3c58089a8e094d472b5
Gulshan Rana <39074166+gulshanrana10@users.noreply.github.com>
2022-04-12 09:07:16 +0530

    Time: 8 ms (36.01%), Space: 8 MB (55.31%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.cpp
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.cpp
@@ -7,16 +7,12 @@ public:
             mpr[indices[i][0]]++;
             mpc[indices[i][1]]++;
         }
-        int ret=0;
-        vector<vector<int>> ans( m, vector<int>(n,0) );
-        
+        int ret=0;        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                ans[i][j]=mpr[i]+mpc[j];
-                if(ans[i][j]%2!=0)ret++;
+                if((mpr[i]+mpc[j])%2!=0)ret++;
             }
         }
-      
         return ret;
         
     }
