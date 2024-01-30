61c6faafaee01114ba0307e871d6ced4c74a85b3
Gulshan Rana <39074166+gulshanrana10@users.noreply.github.com>
2024-01-30 13:08:46 +0530

    Time: 6 ms (64.77%) | Memory: 17.7 MB (84.11%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -11,6 +11,39 @@
  */
 class Solution {
 public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)return 0;
+        queue<pair<TreeNode*, int>> q;
+        int ans=0;
+        q.push({root,0});
+        while(!q.empty()){
+            ans++;
+            int l=q.size();
+            for(int i=0;i<l;i++){
+            TreeNode* tmp=q.front().first;
+            int dist=q.front().second;
+            q.pop();
+            if(tmp->left!=NULL)q.push({tmp->left,dist+1});
+            if(tmp->right!=NULL)q.push({tmp->right,dist+1});
+            }
+        }
+        return ans;
+    }
+};
+/*
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ /*
+class Solution {
+public:
 int ans=0;
     void process(TreeNode* root, int depth){
         if(root){
@@ -24,3 +57,4 @@ int ans=0;
         return ans;
     }
 };
+*/
